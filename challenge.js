dfd2266cb76d52885a809ad6f55377063ac9d0d3
Alden <alden.hari@gmail.com>
2021-09-10 21:06:52 +0700

    fixing test case; implementing solution

--- challenge.js
+++ challenge.js
@@ -1,10 +1,26 @@
 function minMax(arrayNumber) {
-    let sumMin = 0;
-    let sumMax = 0;
-
-    //Write your code below
+    /** MECHANISM
+     *  - iterating according inputted array length
+     *  - for each loop do
+     *  * get sum of all elements except the current pointer (index)
+     *  * check if smaller then sumMin then set the sumMin with new value
+     *  * check if bigger then sumMax then set the sumMax with new value 
+     */
+    let sumMin = null;
+    let sumMax = null;
     
-
+    for (let index = 0; index < arrayNumber.length; index++) {
+        // const element = arrayNumber[index];
+        const currSum = arrayNumber.filter((rec,currIndex)=>currIndex!==index).reduce((acc,currVal)=>{
+            return acc+currVal;
+        });
+        
+        if(!sumMin) sumMin = currSum;
+        if(!sumMax) sumMax = currSum;
+        if(currSum<sumMin) sumMin = currSum;
+        if(currSum>sumMax) sumMax = currSum;
+    }
+    
     return { "sumMin": sumMin, "sumMax": sumMax };
 }
 
